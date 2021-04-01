df58babfa44b28b5425bc830e2efc2841126cfaf
tnieuwe <40697504+tnieuwe@users.noreply.github.com>
2021-04-01 15:50:23 -0400

    initial commit

---
+++ global_in/general_scripts.R
@@ -0,0 +1,10 @@
+## Generic functions used throughout the project
+
+
+sampids <- stab$SAMPID
+
+GTEx_SAMPID_to_SUBJIUD <- function(sampids){
+    ## A simple function to quickly turn SAMPIDs into SUBJIDs to connect
+    ## individuals to their phenotype
+  stringr::str_extract(string = sampids, pattern = "GTEX-[[:alnum:]]*")
+}
